cdd8597b3aa38f4e40cf52ae4bbd36565498daec
isomdurm <isom@isom.tech>
2019-11-07 09:57:40 -0800

    adding in requirements for frontend react components, reducers, etc

--- db/migrate/20191107174418_create_users.rb
+++ db/migrate/20191107174418_create_users.rb
@@ -1,24 +1,25 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :username
-      t.date :date_of_birth
-      t.string :first_name
-      t.string :last_name
-      t.string :email
-      t.string :encrypted_password
-      t.string :session_token
-      t.string :gender
-      t.string :height
-      t.string :weight
-      t.string :street
-      t.string :city
-      t.string :state
-      t.string :zipcode
+      t.date :date_of_birth, null: false
+      t.string :username, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.string :encrypted_password, null: false
+      t.string :session_token, null: false
+      t.string :gender, null: false
+      t.string :height, null: false
+      t.string :weight, null: false
+      t.string :street, null: false
+      t.string :city, null: false
+      t.string :state, null: false
+      t.string :zipcode, null: false
 
       t.timestamps
     end
     add_index :users, :email, unique: true
+    add_index :users, :username, unique: true
     add_index :users, :session_token, unique: true
   end
 end
