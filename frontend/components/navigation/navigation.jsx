19eb3403e0585814a22e882350e6f370afe56b8f
isomdurm <isom@isom.tech>
2019-11-08 10:24:04 -0800

    navbar styling complete

--- frontend/components/navigation/navigation.jsx
+++ frontend/components/navigation/navigation.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-const src = '<%= asset_path "strida.png" %>';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 
 
 const Navigation = ({ currentUser, signin, signout }) => {
   
-  const signoutNavigation = () => (
-    <nav>
-		<img src="/images/strida.png" height="20" width="95"/>
-		<button onClick={signout}>Sign Out</button>
-	</nav>
-  );
+ //  const signoutNavigation = () => (
+ //    <nav>
+	// 	<img src="/images/strida.png" height="20" width="95"/>
+	// 	<button onClick={signout}>Sign Out</button>
+	// </nav>
+ //  );
 
   const signinNavigation = () => (
-    <nav>
-		<img src="/images/strida.png" height="20" width="95"/>
-		<a href="#/signin"><button>Sign In</button></a>
-	</nav>
+    <Navbar>
+      <Navbar.Brand href="#home">
+        <img id="logo-image" src={window.logoURL} height="20" width="95"/>
+      </Navbar.Brand>
+      
+      <Navbar.Toggle />
+        
+      <Navbar.Collapse className="justify-content-end">
+          <a href="#/signin"><Button id="signin-button" size="sm">Sign In</Button></a>
+      </Navbar.Collapse>
+    </Navbar>
   );
 
-  return currentUser ? signoutNavigation() : signinNavigation();
+  return signinNavigation();
 };
 
 
