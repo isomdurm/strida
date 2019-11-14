b90d80c120bfe8b19d867c5378c0556b73e526f4
isomdurm <isom@isom.tech>
2019-11-13 21:33:14 -0800

    posts schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_052304) do
+ActiveRecord::Schema.define(version: 2019_11_14_053100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,18 @@ ActiveRecord::Schema.define(version: 2019_11_14_052304) do
     t.index ["post_id", "user_id"], name: "index_likes_on_post_id_and_user_id", unique: true
     t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "image_url"
+    t.string "address"
+    t.text "description"
+    t.integer "route_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["route_id"], name: "index_posts_on_route_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "routes", force: :cascade do |t|
